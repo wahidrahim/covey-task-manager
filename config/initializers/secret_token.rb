4a86244869564d7871f2766ff0da2b042a179d94
wahidrahim <wahidrahim@gmail.com>
2014-12-08 15:20:54 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoveyTaskManager::Application.config.secret_key_base = '56eca5bc422ddf6594a59b0dcbe2f92da13ebc8ee91d5e1c18b1705c60c82b7a6c4d1d6038b1f1518cbc2a958fea4bf92b6d1fff652488c6428098ecb0a322f1'
